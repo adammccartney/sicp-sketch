b596c47e093d253523340e0926520da886cf11d8
Adam McCartney <mccartney@mdw.ac.at>
2022-01-17 14:39:20 +0100

    Very simple recursion, wobbly solution for pascal

--- src/expressions.scm
+++ src/expressions.scm
@@ -28,6 +28,7 @@
 (sqrt 2)
 (sqrt 4)
 
+
 #| Define a procedure to compute all elements of Pascal's triangle 
  | by means of a recursive process
  | assumes exponent is operating on (x + y) 
@@ -35,6 +36,85 @@
  | (x + y)^1 = x + y 
  |#
 
+(define (len lst)
+  (cond ((null? lst) 0)
+        (else (+ 1 (len (cdr lst))))))
+
+(len (list 1 2 1))
+
+(define (makelist n)
+  (if (= n 0) 
+    (list)
+    (cons 1 (makelist (- n 1)))))
+
+(makelist 5)
+
+
+#| very simple recursion |#
+(define (minusone n)
+  (cond ((= n 0) 1)
+        (else (display n)
+          (minusone (- n 1)))))
+
+(minusone 5)
+
+(cons 1 (+ (car (list 1 2 1)) (car (cdr (list 1 2 1)))))
+
+
+#| add an element to a list |#
+(define (extend l . xs)
+  (if (null? l)
+    xs
+    (cons (car l) (apply extend (cdr l) xs))))
+
+(extend '(1 2) 1)
+
+#| adds the first and second elements in a list |#
+(define (sumpair lst)
+  (cond ((= (len lst) 1) 1)
+        (else (+ (car lst) (car (cdr lst))))))
+
+(sumpair (list 1 3 6 3 1))
+
+(define (len lst)
+  (cond ((null? lst) 0)
+        (else (+ 1 (len (cdr lst))))))
+
+(define (sumpair-iter lst newlist)
+  (define (sumpair lst)
+    (cond ((= (len lst) 1) 1)
+          (else (+ (car lst) (car (cdr lst))))))
+    (define (len lst)
+      (cond ((null? lst) 0)
+            (else (+ 1 (len (cdr lst))))))
+    (define (extend l . xs)
+      (if (null? l)
+        xs
+        (cons (car l) (apply extend (cdr l) xs))))
+  (cond ((= (len lst) 1)   #| last element, append one |#
+        (extend newlist 1))
+        (else
+          (sumpair-iter (cdr lst) (extend newlist (sumpair lst))))))
+
+#| test |#
+(sumpair-iter (list 1 2 1) (list 1 1))
+
+(define (getlist lst)
+  (define (len lst)
+    (cond ((null? lst) 0)
+          (else (+ 1 (len (cdr lst))))))
+  (if (= (len lst) 1) 
+    (list 1 1))
+    (list 1))
+
+#| need to define a way to create a new list of length len + 1 with all ones
+ | then iterate over this list and replace the middle values with ones
+ | calculated from a previous list 
+ |# 
+
+getlist (list 1 1 1 1 1 1))
+
+(makelist (list 1))
 
 (define (makelevel n)
   (if (= n 0)
