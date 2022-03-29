c50408d5297d43d94d189f2c015b8dd20d1d7c28
adammccartney <adam@mur.at>
2022-03-29 08:29:43 +0200

    Adding some rough work on hierarchical structures
    Specifically focused on a procedure to reverse a list.
    This initial commit includes a shallow reverse.

--- src/abstractions-wdata.scm
+++ src/abstractions-wdata.scm
@@ -25,7 +25,7 @@
 #| Exercise 2.23
  | The procedure for-each is similar to map. It takes as arguments the
  | procedure and a list of elements. However, rather than formatting a list of
- | the results, for-each just applires the procuedure to each of the elements
+ | the results, for-each just applies the procuedure to each of the elements
  | in turn, from left to right. The value returned by applying the procedure to
  | the elements are not used at all -- for-each is used with prcedures that
  | perform an action, such as printing. For example:
@@ -46,3 +46,108 @@
 
 (for-each (lambda (x) (newline) (display x))
           (list 57 321 88))
+
+
+#| Exercise 2.25 
+ | Give combinations of cars and cdrs that will pick 7 from each of the
+ | following lists
+ |#
+
+(define odd (list 1 3 (list 5 7) 9))
+(define seven (list (list 7)))
+(define sixlists (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+
+
+(cdr 
+  (car 
+    (cdr (cdr odd))))
+
+(car seven)
+
+(cdr 
+  (car 
+    (cdr 
+      (car 
+        (cdr 
+          (car 
+            (cdr 
+              (car 
+                (cdr 
+                  (car 
+                    (cdr sixlists)))))))))))
+
+
+#| Exercise 2.26 
+ | Suppose we define x and y  to be two lists:
+ | what rexult is printed by the interpreter given the expressions that follow 
+ |#
+
+(define x (list 1 2 3))
+
+(define y (list 4 5 6))
+
+(append x y)
+
+(cons x y)
+
+(list x y)
+
+#| Exercise 2.18
+ | Define a procedure reverse that takes a list as arugment and returns a list
+ | of the same elements in reverse order:
+ | (reverse (list 1 4 9 16 25))
+ | (25 16 9 4 1)
+ |#
+
+(define alist (list ))
+
+(display alist)
+
+(define items (list 1 2 3 4 5))
+
+(define (length items)
+  (if (null? items)
+    0
+    (+ 1 (length (cdr items)))))
+
+(define (list-ref items n)
+  (if (= n 0)
+    (car items)
+    (list-ref (cdr items) (- n 1))))
+
+(length items)
+
+(define rlist (list))
+
+(append (list (append rlist (list-ref items (- (length items) 1)))) (cdr
+                                                                      items))
+
+(cdr items)
+
+(define nil '())
+
+(define (reverse items) 
+  (define (iter items result) 
+    (if (null? items) 
+        result 
+        (iter (cdr items) (cons (car items) result)))) 
+  (iter items nil)) 
+
+(define squares (list 1 4 9 16 25))
+
+(reverse squares)
+
+#| Exercise 2.27
+ | Modify your reverse procedure of exercise 2.18 to produce a deep-reverse
+ | procedure that takes a list as argument and returns as its value the list
+ | with its elements reversed and with all sublists deep-reversed as well. For
+ | example:
+ | (define x (list (list 1 2) (list 3 4)))
+ | x 
+ | ((1 2) (3 4))
+ | (reverse x)
+ | ((3 4) (1 2))
+ | (deep-reverse x)
+ | ((4 3) (2 1))
+ |#
+
