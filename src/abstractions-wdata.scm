8bef27a7eecdbb6eb38ca92a980c5048fb8ddf45
adammccartney <adam@mur.at>
2022-04-10 17:24:42 +0200

    Adding simple exercise solution on trees
    Just some simple practice on the way to learning maps, filters and
    accumulations

--- src/abstractions-wdata.scm
+++ src/abstractions-wdata.scm
@@ -283,4 +283,96 @@
 
 
 
+#| Exercise 2.29 A binary mobile consists of two branches,
+ | a le branch and a right branch. Each branch is a rod of
+ | a certain length, from which hangs either a weight or another binary mobile. 
+ | We can represent a binary mobile using compound data by constructing it from two branches
+ | (for example, using list):
+ | (define (make-mobile left right)
+ | (list left right))
+ | A branch is constructed from a length (which must be a
+ | number) together with a structure, which may be either a
+ | number (representing a simple weight) or another mobile:
+ | (define (make-branch length structure)
+ | (list length structure))
+ |
+ | a. Write the corresponding selectors left-branch and
+ | right-branch, which return the branches of a mobile,
+ | and branch-length and branch-structure, which return the components of a branch.
+ |
+ | b. Using your selectors, define a procedure total-weight
+ | that returns the total weight of a mobile.
+ |
+ | c. A mobile is said to be balanced if the torque applied by
+ | its top-le branch is equal to that applied by its top
+ | right branch (that is, if the length of the le rod multiplied by the weight hanging from that rod is equal
+ | to the corresponding product for the right side) and if
+ | each of the submobiles hanging off its branches is balanced. Design a predicate that tests whether a binary
+ | mobile is balanced.
+ |
+ | d. Suppose we change the representation of mobiles so
+ | that the constructors are
+ | (define (make-mobile left right) (cons left right))
+ | (define (make-branch length structure)
+ | (cons length structure))
+ | How much do you need to change your programs to
+ | convert to the new representation?
+ |#
 
+#| constructors |#
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch len struct)
+  (list len struct))
+
+
+#| selectors |#
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (car (reverse mobile)))
+
+(define (branch-weight branch)
+  (car (reverse branch)))
+
+#| selects the vector (torque) of a branch |#
+(define (vec branch)
+  (* (car branch) (branch-weight branch)))
+
+(vec abranch)
+
+(define abranch (make-branch 10 15))
+(define bbranch (make-branch 5 6))
+
+(* (car abranch) (branch-weight abranch))
+
+(define mymobile (make-mobile abranch bbranch))
+
+(left-branch mymobile)
+(right-branch mymobile)
+
+#| define procedure total-weight |#
+(define (total-weight mobile)
+  (define (iter mobile sum)
+    (if (null? mobile) 
+      sum
+      (iter (cdr mobile) (+ sum (branch-weight (car mobile))))))
+    (iter mobile 0))
+
+(total-weight mymobile)
+
+
+#| check if mobile is balanced |#
+(define (balanced? mobile)
+  (define lvec (vec (left-branch mobile)))
+  (define rvec (vec (right-branch mobile)))
+  (equal? lvec rvec))
+
+(define lbranch (make-branch 4 6))
+(define rbranch (make-branch 8 3))
+(define bmob (make-mobile lbranch rbranch))
+
+(balanced? mymobile)
+(balanced? bmob)
